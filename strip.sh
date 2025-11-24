6d4eb184edf6f7cee9c3c133481f5f27121a59c2
ahaoboy <504595380@qq.com>
2025-11-24 10:23:02 +0800

    strip: skip quickjs-emscripten-cli

--- strip.sh
+++ strip.sh
@@ -17,7 +17,7 @@ case "$(uname -s)" in
   *MINGW*|*MSYS*|*CYGWIN*) is_windows=true ;;
 esac
 
-skip_list=("goja" jint-cli)
+skip_list=("goja" "jint-cli" "quickjs-emscripten-cli")
 
 jq -c '.[]' "$json_file" | while IFS= read -r item; do
   name=$(echo "$item" | jq -r '.bin // .name')
